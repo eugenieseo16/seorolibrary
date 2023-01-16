bbd73d06ac138841e40d11acf223314aed109369
이지영 <jylisa0612@naver.com>
2023-01-16 15:32:27 +0900

    :tada: init : init spring project
    init : init spring project

---
+++ BE/src/test/java/com/ssafy/udangtang/UdangtangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.udangtang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdangtangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
