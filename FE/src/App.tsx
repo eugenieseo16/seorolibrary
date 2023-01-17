6b7f01ed567c6774da3026d868d865ff9a3753c8
hi6724 <hi6724@gmail.com>
2023-01-17 16:51:22 +0900

    🐛 fix : 없는 페이지 생성

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import { Home } from "./pages/Home";
-//import { NotFound } from "./pages/NotFound";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
