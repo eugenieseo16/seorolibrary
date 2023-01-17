e61c2c3e36b9188514a217fcba04629ccb2d38c2
하훈목 <hunmok1027@gmail.com>
2023-01-17 17:33:21 +0900

    ♻️ refactor : 리팩토링

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import NotFound from './pages/NotFound';
+import Router from './Router';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Router />
     </div>
   );
 }
