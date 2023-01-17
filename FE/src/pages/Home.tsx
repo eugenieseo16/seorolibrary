9c0b21e0409c4e90c80e0057601e5ab66fb055f2
하훈목 <hunmok1027@gmail.com>
2023-01-17 17:52:02 +0900

    🧪 test : redux 테스트 코드

--- FE/src/pages/Home.tsx
+++ FE/src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+
+import { logout } from '@src/store/slices/userSlice';
 
 function Home() {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+  return (
+    <div>
+      <button type="button" onClick={handleLogout}>
+        Logout
+      </button>
+    </div>
+  );
 }
 
 export default Home;
