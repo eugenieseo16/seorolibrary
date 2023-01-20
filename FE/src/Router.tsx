4502c4bb7013d8e5540c1d68f7390826b0c8de8a
yjohbjects <yjohbjects@gmail.com>
2023-01-20 17:28:49 +0900

    🔥 remove : login, signup 삭제

--- FE/src/Router.tsx
+++ FE/src/Router.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from '@pages/Home';
-import Login from '@pages/Login';
+// import Login from '@pages/Login';
 // import NotFound from '@pages/NotFound';
-import Signup from '@pages/Signup';
+// import Signup from '@pages/Signup';
 import { useSelector } from 'react-redux';
 
 function Router() {
@@ -12,8 +12,8 @@ function Router() {
   return (
     <Routes>
       <Route path="/" element={user ? <Home /> : <Login />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+      {/* <Route path="/login" element={<Login />} /> */}
+      {/* <Route path="/signup" element={<Signup />} /> */}
       {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
