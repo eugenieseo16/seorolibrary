5e6af2d4e03ee1fe6d53adde50059e2ba780f74b
하훈목 <hunmok1027@gmail.com>
2023-01-25 23:06:29 +0900

    🐛 fix : Login 페이지가 없던 버그 수정

--- FE/src/Router.tsx
+++ FE/src/Router.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Home from '@pages/Home';
-// import Login from '@pages/Login';
-// import NotFound from '@pages/NotFound';
-// import Signup from '@pages/Signup';
-import { useSelector } from 'react-redux';
+import Login from '@pages/Login';
 
 function Router() {
   const user = useSelector((state: any) => state.user);
   return (
     <Routes>
       <Route path="/" element={user ? <Home /> : <Login />} />
-      {/* <Route path="/login" element={<Login />} /> */}
-      {/* <Route path="/signup" element={<Signup />} /> */}
-      {/* <Route path="*" element={<NotFound />} /> */}
     </Routes>
   );
 }
