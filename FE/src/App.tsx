641cf828d5b40a24023f5ac609a2f3af3813de47
하훈목 <hunmok1027@gmail.com>
2023-01-25 23:38:42 +0900

    ✨ feat : font 로딩 추가

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
+
 import Router from './Router';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const loadFonts = async () => {
+    await new FontFaceObserver('BM-Pro').load();
+    setLoading(false);
+  };
+  useLayoutEffect(() => {
+    loadFonts();
+  }, []);
   return (
-    <div className="App">
-      <Router />
+    <div className="App" style={{ fontFamily: 'BM-Pro' }}>
+      {loading ? 'Loading...' : <Router />}
     </div>
   );
 }
