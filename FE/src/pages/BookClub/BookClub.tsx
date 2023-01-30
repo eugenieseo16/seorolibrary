526c3270b795ecaaea8e5752d9f88923b92d5dd7
하훈목 <hunmok1027@gmail.com>
2023-01-30 16:04:18 +0900

    ✨ feat : book-club 페이지 모임추천 작성

--- FE/src/pages/BookClub/BookClub.tsx
+++ FE/src/pages/BookClub/BookClub.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+import Recommend from '@components/Recommend/Recommend';
+import './BookClub.styles.scss';
 
 function BookClub() {
-  return <div>BookClub</div>;
+  return (
+    <div>
+      <div>헤더</div>
+      <div>캐러셀</div>
+      <div>독서모임 만들기</div>
+      <Recommend />
+    </div>
+  );
 }
 
 export default BookClub;
