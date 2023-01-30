b20279bf2edd1c206563eae8ead0ef10b026d466
OYJ <yeonjinnieoh@gmail.com>
2023-01-30 16:43:58 +0900

    🌱 add : user profile 페이지 추가

--- FE/src/Router.tsx
+++ FE/src/Router.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import Home from '@pages/Home';
+import UserLibrary from '@pages/UserLibrary';
 import Login from '@pages/Login';
 import WithNavLayout from '@pages/withNavLayout/WithNavLayout';
 
@@ -12,6 +13,7 @@ function Router() {
     <Routes>
       <Route path="" element={<WithNavLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<UserLibrary />} />
         <Route path="*" element={'404'} />
       </Route>
     </Routes>
