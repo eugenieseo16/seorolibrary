d71a44bcb1f24a7d92bb650652ffbe7ce96c4bab
이지영 <jylisa0612@naver.com>
2023-01-30 10:29:22 +0900

    :tada: init : init spring project
    init : init spring project

---
+++ BE/src/main/java/com/seoro/seoro/SeoroApplication.java
@@ -0,0 +1,13 @@
+package com.seoro.seoro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoroApplication.class, args);
+	}
+
+}
