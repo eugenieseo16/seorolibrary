1740472fb3cdbe4bb61ce19699ac8b224504aef8
하훈목 <hunmok1027@gmail.com>
2023-01-31 10:19:10 +0900

    ✨ feat : react-query 로 변경 & auto play

--- FE/src/components/Carousel/Carousel.tsx
+++ FE/src/components/Carousel/Carousel.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Slider from 'react-slick';
+import { useQuery } from 'react-query';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -10,22 +11,20 @@ const settings = {
   speed: 500,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  pauseOnHover: true,
 };
 
 export default function Carousel() {
-  const [recommendData, setRecommendData] = useState<any>();
-  const getRecommendData = async () => {
-    const { data } = await (await fetch('/clubRecommend.json')).json();
-    setRecommendData(data);
-  };
+  const getRecommendData = async () =>
+    await (await fetch('/clubRecommend.json')).json();
 
-  useEffect(() => {
-    getRecommendData();
-  }, []);
+  const { data } = useQuery('club-recommend', getRecommendData);
 
   return (
     <Slider {...settings} className="my-slider">
-      {recommendData?.map((data: any, i: number) => (
+      {data?.data?.map((data: any, i: number) => (
         <div className="carousel-container">
           <img src={data.image_url} alt="" />
           <div className="shadow-wrapper" />
