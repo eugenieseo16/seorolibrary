e648097badde85b5e278718dd7063d85e2df3581
하훈목 <hunmok1027@gmail.com>
2023-01-31 17:22:58 +0900

    ✨ feat : ToggleNav 명확하게 변경

--- FE/src/components/Recommend/Recommend.tsx
+++ FE/src/components/Recommend/Recommend.tsx
@@ -2,21 +2,24 @@ import React, { useState } from 'react';
 
 import ClubRecommend from '@components/ClubRecommend/ClubRecommend';
 import UserRecommend from '@components/UserRecommend/UserRecommend';
-import './Recommend.styles.scss';
 import ToggleNav from '@components/ToggleNav/ToggleNav';
+import './Recommend.styles.scss';
 
 function Recommend() {
-  const [selectedId, setSelectedId] = useState('0');
+  const [selectedId, setSelectedId] = useState('user');
 
   return (
     <div className="recommend-container">
       <ToggleNav
         selectedId={selectedId}
         setSelectedId={setSelectedId}
-        items={['이용자추천', '모임추천']}
+        items={[
+          { text: '이용자추천', id: 'user' },
+          { text: '모임추천', id: 'club' },
+        ]}
       />
       {/* users 인경우는 react-slick 써서 캐러셀로 */}
-      {selectedId === '0' ? <UserRecommend /> : <ClubRecommend />}
+      {selectedId === 'user' ? <UserRecommend /> : <ClubRecommend />}
     </div>
   );
 }
