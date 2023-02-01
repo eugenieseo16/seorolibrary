0b91016d5f9d9280b4d1f332df39f054315d2fba
yeonjinnieoh <yeonjinnieoh@gmail.com>
2023-02-01 21:26:19 +0900

    ✨ feature : user library 하위 페이지 구현

--- FE/src/pages/UserLibrary/UserLibrary.tsx
+++ FE/src/pages/UserLibrary/UserLibrary.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import SearchHeader from '@components/SearchHeader/SearchHeader';
+import UserHeader from '@components/UserLibrary/UserHeader';
 import UserProfile from '@components/UserLibrary/UserSection/UserProfile';
 import UserStat from '@components/UserLibrary/UserStat';
 import BookTab from '@components/UserLibrary/BookTab';
@@ -10,7 +11,11 @@ import './UserLibrary.styles.scss';
 function UserLibrary() {
   return (
     <div className="user-library-container">
-      <SearchHeader text="나의 도서관" />
+      {/* api 연결되면 userId로 구분해서 구현할 예정 */}
+      {/* 타 사용자의 도서관일 경우 */}
+      {/* <SearchHeader text="나의 도서관" /> */}
+      {/* 나의 도서관일 경우 */}
+      <UserHeader/>
       <UserProfile />
       <UserStat />
       <BookTab />
