d4bbb8c40c2f388ca04f7d8f597e02ccc382020b
yjohbjects <yjohbjects@gmail.com>
2023-02-01 16:00:10 +0900

    ✨ feature : book detail 페이지 구현

--- FE/src/pages/BookDetail/BookDetail.tsx
+++ FE/src/pages/BookDetail/BookDetail.tsx
@@ -4,11 +4,20 @@ import SearchHeader from '@components/SearchHeader/SearchHeader';
 import BookInfo from '@components/BookInfo/BookInfo';
 import BookReview from '@components/BookInfo/BookReview';
 
+import './BookDetail.styles.scss';
+
 function BookDetail() {
   return (
-    <div>
+    <div className="book-detail-container">
       <SearchHeader text="도서 정보" />
       <BookInfo />
+      <h1>(재사용 가능한 읽은 유저수, 한줄평, 리뷰칸들로 바꾸는중입니다)</h1>
+      {/* 사용자의 보유도서 상세를 클릭한 경우 */}
+      <div>보유도서</div>
+      <p>carousel</p>
+      {/* 표준 도서상세 */}
+      <div>보유사용자</div>
+      <p>carousel</p>
       <BookReview />
     </div>
   );
