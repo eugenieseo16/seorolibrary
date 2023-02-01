7f56d9b537e5dffa0f47a1d83773245c613beb1d
OYJ <yeonjinnieoh@gmail.com>
2023-02-01 14:49:43 +0900

    ✨ feature : book detail 페이지 구현

--- FE/src/pages/BookDetail/BookDetail.tsx
+++ FE/src/pages/BookDetail/BookDetail.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-function BookDetail () {
+import SearchHeader from '@components/SearchHeader/SearchHeader';
+import BookInfo from '@components/BookInfo/BookInfo';
+import BookReview from '@components/BookInfo/BookReview';
+
+function BookDetail() {
   return (
-    <div>Book Detail
-      <h1>도서 검색했을 때 나오는 도서 상세페이지</h1>
-      <h1>책정보</h1>
-      <h1>리뷰</h1>
-      <h1>근처 보유 사용자</h1>
+    <div>
+      <SearchHeader text="도서 정보" />
+      <BookInfo />
+      <BookReview />
     </div>
-  )
+  );
 }
 
 export default BookDetail;
