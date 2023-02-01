bf46529497b3764b4ce41e98801e0c5c98a31019
윤혜진 <hjyoon0223@gmail.com>
2023-02-01 17:07:36 +0900

    ✨ feat: book 관련 dto 생성

--- BE/src/main/java/com/seoro/seoro/domain/dto/Library/RentBookDto.java
+++ BE/src/main/java/com/seoro/seoro/domain/dto/Library/RentBookDto.java
@@ -1,4 +1,18 @@
 package com.seoro.seoro.domain.dto.Library;
 
+import java.util.Date;
+
+import com.seoro.seoro.domain.entity.ChatRoom.ChatRoom;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class RentBookDto {
+	private Long rendBookId;
+	private ChatRoom chatRoom;
+	private String isbn;
+	private Date rendDate;
+	private Boolean isReturn;
 }
