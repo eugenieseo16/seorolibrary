a7a39b4d6336a8b7b9f28265144c95b7a6ec0913
윤혜진 <hjyoon0223@gmail.com>
2023-02-02 15:00:55 +0900

    ✨ feat: library controller, service 추가

--- BE/src/main/java/com/seoro/seoro/domain/dto/User/UserProfileDto.java
+++ BE/src/main/java/com/seoro/seoro/domain/dto/User/UserProfileDto.java
@@ -1,16 +1,20 @@
 package com.seoro.seoro.domain.dto.User;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserProfileDto {
 	private Long userId;
 	private String userName;
 	private String userProfile;
 	private String userDongCode;
 	private Integer userScore;
-	private Integer followerCnt;
-	private Integer followingCnt;
+	private Long followerCnt;
+	private Long followingCnt;
 }
