4912f7d1186580c9e38cc8625cc84b68dec9e6db
EveKritinLee <jylisa0612@naver.com>
2023-02-03 01:14:31 +0900

    :sparkles: feat:독서그룹 게시판 작성 구현
    feat:독서그룹 게시판 작성 구현

---
+++ BE/src/main/java/com/seoro/seoro/repository/GroupPost/GroupPostRepository.java
@@ -0,0 +1,9 @@
+package com.seoro.seoro.repository.GroupPost;
+
+import com.seoro.seoro.domain.entity.Groups.GroupPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupPostRepository extends JpaRepository<GroupPost, Long> {
+}
