33fe935907b0aeb2eddd6551d1da407bd1e99d97
yjohbjects <yjohbjects@gmail.com>
2023-02-02 16:17:05 +0900

    ✨ feature : book register 구현

--- FE/src/components/BookInfo/BookDetailHeader.tsx
+++ FE/src/components/BookInfo/BookDetailHeader.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { RiArchiveLine, RiSettings4Line } from 'react-icons/ri';
+import { RiAddLine } from 'react-icons/ri';
 
-import './UserHeader.styles.scss';
+import './BookDetailHeader.styles.scss';
 
-function UserHeader() {
+function BookDetailHeader() {
   const navigate = useNavigate();
   const onClickProfileSettings = () => {
     navigate(`settings`);
@@ -18,19 +18,15 @@ function UserHeader() {
   return (
     <div className="user-header-container">
       <div className="user-header-text">
-        <h1>나의 도서관</h1>
+        <h1>도서 정보</h1>
       </div>
       <div className="header-icon-container">
         <div className="header-icon-item">
-          <RiArchiveLine onClick={onClickMyArchive} size={'2rem'} />
-        </div>
-
-        <div className="header-icon-item">
-          <RiSettings4Line onClick={onClickProfileSettings} size={'2rem'} />
+          <RiAddLine onClick={onClickMyArchive} size={'2rem'} />
         </div>
       </div>
     </div>
   );
 }
 
-export default UserHeader;
+export default BookDetailHeader;
