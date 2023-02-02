dcea76bd5d0744923ba2108179e93cf36aa8ce61
OYJ <yeonjinnieoh@gmail.com>
2023-02-02 16:17:05 +0900

    ✨ feature : book register 구현

--- BE/src/main/java/com/seoro/seoro/repository/Book/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.seoro.seoro.repository.Book;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.seoro.seoro.domain.entity.Book.Book;
-
-public interface BookRepository extends JpaRepository<Book, String> {
-}
