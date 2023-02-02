5f6aabb14b602ce6eb069fe025ef1909224f7175
OYJ <yeonjinnieoh@gmail.com>
2023-02-02 17:38:41 +0900

    ✨ feature : book register 페이지 구현

--- FE/src/components/ExchangeAvailable.tsx
+++ FE/src/components/ExchangeAvailable.tsx
@@ -5,14 +5,18 @@ import { Divider, Space, Tag } from 'antd';
 import './UserAddressToggle.styles.scss';
 
 const ExchangeAvailable = () => {
+  const is_available = Math.floor(Math.random() * 2);
   return (
     <div>
-      <Tag icon={<CheckCircleOutlined />} color="#31C454">
-        바꿔읽기 가능
-      </Tag>
-      <Tag icon={<CloseCircleOutlined />} color="error">
-        바꿔읽기 불가
-      </Tag>
+      {is_available ? (
+        <Tag icon={<CheckCircleOutlined />} color="#31C454">
+          바꿔읽기 가능
+        </Tag>
+      ) : (
+        <Tag icon={<CloseCircleOutlined />} color="error">
+          바꿔읽기 불가
+        </Tag>
+      )}
     </div>
   );
 };
