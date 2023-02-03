58412756d4080b8eaede48390585d73f604244a9
이지영 <jylisa0612@naver.com>
2023-02-03 16:46:19 +0900

    :bug: fix: BookService Annotation 추가
    fix: BookService Annotation 추가

--- BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
+++ BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
@@ -3,7 +3,10 @@ package com.seoro.seoro.service.Book;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.seoro.seoro.domain.dto.Book.BookDto;
+@Service
 public interface BookService {
 
 	List<BookDto> findAllBooks();
