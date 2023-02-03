960db9420139dac714879dca69ba0a940176d221
yujinseo <00yujin00@naver.com>
2023-02-03 10:59:31 +0900

    ⚡️ feat : 장소추천 연관 페이지 추가

--- FE/src/Router.tsx
+++ FE/src/Router.tsx
@@ -15,15 +15,17 @@ import BookRegister from '@pages/BookRegister/BookRegister';
 
 import Login from '@pages/Login';
 import WithNavLayout from '@pages/withNavLayout/WithNavLayout';
-import BookClub from '@pages/BookClub/BookClub';
-import Places from '@pages/Places/Places';
-import PlaceDetail from '@pages/PlaceRecommend/PlaceDetail';
-import BookClubDetail from '@pages/BookClubDetail/BookClubDetail';
 
+import BookClub from '@pages/BookClub/BookClub';
+import BookClubDetail from '@pages/BookClubDetail/BookClubDetail';
 import BookClubGenerate from '@pages/BookClubGenerate/BookClubGenerate';
 
-import Near from '@pages/Near/Near';
+import Places from '@pages/Places/Places';
+import PlaceDetail from '@pages/PlaceRecommend/PlaceDetail';
+import AddPlace from '@pages/PlaceRecommend/AddPlace';
+import MyPlace from '@pages/PlaceRecommend/MyPlace';
 
+import Near from '@pages/Near/Near';
 
 function Router() {
   const user = useSelector((state: any) => state.user);
@@ -31,29 +33,28 @@ function Router() {
     <Routes>
       <Route path="" element={<WithNavLayout />}>
         <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<BookDetail/>} />
+        <Route path="/book/:id" element={<BookDetail />} />
 
         <Route path="/profile" element={<MyLibrary />} />
         <Route path="/profile/:userId" element={<UserLibrary />} />
-        <Route path="/profile/archive" element={<MyArchive/>} />
+        <Route path="/profile/archive" element={<MyArchive />} />
         <Route path="/profile/log" element={<ProfileLog />} />
-        <Route path="profile/statistics" element={<ProfileStat/>} />
-        
-
+        <Route path="profile/statistics" element={<ProfileStat />} />
         <Route path="/profile/settings" element={<ProfileSettings />} />
         <Route path="/profile/register" element={<BookRegister />} />
 
         <Route path="/book-club" element={<BookClub />} />
-
         <Route path="/book-club/generate" element={<BookClubGenerate />} />
+        <Route path="/book-club/:id" element={<BookClubDetail />} />
 
         <Route path="/places" element={<Places />} />
+        <Route path="/places/:id" element={<PlaceDetail />} />
+        <Route path="/places/myplace" element={<MyPlace />} />
+        <Route path="/places/addplace" element={<AddPlace />} />
+
         <Route path="/near" element={<Near />} />
         <Route path="/near/bookdetail/:id" element={<BookDetail />} />
 
-        <Route path="/book-club/:id" element={<BookClubDetail />} />
-
-        <Route path="/placedetail" element={<PlaceDetail />} />
         <Route path="*" element={'404'} />
       </Route>
     </Routes>
