f43ad333d69d57650af578ec76e64d7d07db33f8
cjfgns <cjfgns1997@naver.com>
2023-02-05 02:19:42 +0900

    :sparkles: Make Review
    Make Review

--- BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
+++ BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
@@ -6,12 +6,18 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.seoro.seoro.domain.dto.Book.BookDto;
+import com.seoro.seoro.domain.dto.Book.ReviewDto;
+import com.seoro.seoro.domain.dto.ResultResponseDto;
+
 @Service
 public interface BookService {
 
 	List<BookDto> findAllBooks();
-	List<BookDto> findByIsbn(String isbn);
+	BookDto findByIsbn(String isbn);
+	List<ReviewDto> findReviewByIsbn(String isbn);
 
 	List<BookDto> findByBookTitleLikeOrBookAuthorLike(String input1, String input2);
 	String findBestSeller() throws IOException;
+
+	ResultResponseDto makeReview(ReviewDto requestDto);
 }
