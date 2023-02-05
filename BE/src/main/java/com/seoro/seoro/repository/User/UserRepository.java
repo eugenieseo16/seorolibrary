63c3937b8f4840a151eb52213e0dd34a43691a8a
cjfgns <cjfgns1997@naver.com>
2023-02-05 20:48:52 +0900

    :zap: integrate search finish
    integrate search finish

--- BE/src/main/java/com/seoro/seoro/repository/User/UserRepository.java
+++ BE/src/main/java/com/seoro/seoro/repository/User/UserRepository.java
@@ -1,5 +1,7 @@
 package com.seoro.seoro.repository.User;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.seoro.seoro.domain.entity.User.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	List<User> findByUserNameLike(String userName);
 	User findUserByUserName(String userName);
 }
