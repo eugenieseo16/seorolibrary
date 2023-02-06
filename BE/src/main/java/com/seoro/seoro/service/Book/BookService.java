fc1a8b1cedcfd62f2f5835cc0434286f94fcfacb
cjfgns <cjfgns1997@naver.com>
2023-02-06 17:30:19 +0900

    :zap: modify bestseller
    modify bestseller

--- BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
+++ BE/src/main/java/com/seoro/seoro/service/Book/BookService.java
@@ -12,13 +12,13 @@ import com.seoro.seoro.domain.dto.ResultResponseDto;
 
 @Service
 public interface BookService {
+	// List<BookDto> findBook(String input);
+	// List<BookDto> findAllBooks();
+	// BookDto findByIsbn(String isbn);
+	// List<ReviewDto> findReviewByIsbn(String isbn);
 
-	List<BookDto> findAllBooks();
-	BookDto findByIsbn(String isbn);
-	List<ReviewDto> findReviewByIsbn(String isbn);
+	// List<BookDto> findByBookTitleLikeOrBookAuthorLike(String input1, String input2);
+	List findBestSeller() throws IOException;
 
-	List<BookDto> findByBookTitleLikeOrBookAuthorLike(String input1, String input2);
-	String findBestSeller() throws IOException;
-
-	ResultResponseDto makeReview(ReviewDto requestDto);
+	// ResultResponseDto makeReview(ReviewDto requestDto);
 }
