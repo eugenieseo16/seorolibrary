619334481fc82f638062785da72d1d0a7a88bdc8
이지영 <jylisa0612@naver.com>
2023-02-06 17:54:04 +0900

    :sparkles: feat : 독서모임 게시판 출력 구현중
    feat : 독서모임 게시판 출력 구현중

--- BE/src/main/java/com/seoro/seoro/repository/GroupPost/GroupPostRepository.java
+++ BE/src/main/java/com/seoro/seoro/repository/GroupPost/GroupPostRepository.java
@@ -1,7 +1,14 @@
 package com.seoro.seoro.repository.GroupPost;
 
+import java.util.List;
+
 import com.seoro.seoro.domain.entity.Groups.GroupPost;
+import com.seoro.seoro.domain.entity.Groups.Groups;
+import com.seoro.seoro.domain.entity.Groups.PostCategory;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -9,4 +16,12 @@ public interface GroupPostRepository extends JpaRepository<GroupPost, Long> {
 
 	@Override
 	void deleteById(Long postId);
+
+	// @Query(value = "select post " +
+	// 		"from GroupPost post " +
+	// 		"where post.groups.groupId = :groupId " +
+	// 	"and post.postCategory = :#{#postCategory.name()} " +
+	// 	"order by post.groupPostTime desc")
+
+	List<GroupPost> findGroupPostsByGroupsAndPostCategoryOrderByGroupPostTimeDesc(Groups groups, PostCategory postCategory);
 }
