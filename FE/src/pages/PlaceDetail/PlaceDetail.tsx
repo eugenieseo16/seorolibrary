63b8a5fbf200e9f8859f5659134de6319163ec4c
yujinseo <00yujin00@naver.com>
2023-02-07 17:19:27 +0900

    ✨ feat : placemap 하는 중

--- FE/src/pages/PlaceDetail/PlaceDetail.tsx
+++ FE/src/pages/PlaceDetail/PlaceDetail.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import PlaceDetailHeader from '@components/PlaceRecommend/PlaceDetailHeader';
 import PlaceMap from '@components/MyPlace/PlaceMap';
+import PlaceImage from '@components/MyPlace/PlaceImage';
 
 import './PlaceDetail.styles.scss';
 
 function PlaceDetail() {
-  return <div className="place-detail-container">
-    <PlaceDetailHeader/>
-    <PlaceMap/>
-  </div>;
+  return (
+    <div className="place-detail-container">
+      <PlaceDetailHeader />
+      <div>
+        <PlaceMap />
+        <PlaceImage />
+      </div>
+    </div>
+  );
 }
 
 export default PlaceDetail;
