d1304df1fe45066d9ba14d118b8b2134597089a8
yeonjinnieoh <yeonjinnieoh@gmail.com>
2023-02-08 21:54:43 +0900

    ✨ feature : book detail 페이지 구현

--- FE/src/pages/BookDetail/BookDetail.tsx
+++ FE/src/pages/BookDetail/BookDetail.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 import BookDetailHeader from '@components/BookInfo/BookDetailHeader';
 import BookInfo from '@components/BookInfo/BookInfo';
@@ -11,19 +12,33 @@ import HoldUser from '@components/BookInfo/Carousel/HoldUser'
 import './BookDetail.styles.scss';
 
 function BookDetail() {
+  const location = useLocation();
+
+  // console.log(location.pathname);
+
+  const isUser = (location.pathname).includes("profile");
+
   return (
     <div className="book-detail-container">
+      <h1>{location.pathname}</h1>
       <BookDetailHeader />
       {/* <SearchHeader text="도서 정보" /> */}
       <BookInfo />
       <BookStat />
-      
-      {/* 사용자 도서 상세- 사용자의 보유도서 캐러셀*/}
-      <HoldBook/>
+      { isUser ? (
+        <div>
+          {/* 사용자 도서 상세- 사용자의 보유도서 캐러셀*/}
+          <HoldBook/>
+          
+        </div>
+      ) : (
+        <div>
+          {/* 표준 도서상세- 근처 도서 보유 사용자 캐러셀 */}
+          <HoldUser/>
+        </div>
+      )}
       
 
-      {/* 표준 도서상세- 근처 도서 보유 사용자 캐러셀 */}
-      <HoldUser/>
       
       <BookReview />
     </div>
