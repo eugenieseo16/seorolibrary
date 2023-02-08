348881c005fbc24514b0bc11f985109c8ab79d19
하훈목 <hunmok1027@gmail.com>
2023-02-08 09:39:04 +0900

    ✨ feat : 모임 읽은책

--- FE/src/pages/ClubBooks/ClubBooks.tsx
+++ FE/src/pages/ClubBooks/ClubBooks.tsx
@@ -1,10 +1,29 @@
 import ClubBottomNav from '@components/BottomNav/ClubBottomNav';
+import SearchHeader from '@components/SearchHeader/SearchHeader';
+import { useMyQuery } from '@src/hooks/useMyQuery';
 import React from 'react';
+import './ClubBooks.styles.scss';
 
+interface IBook {
+  id: string;
+  image_url: string;
+  title: string;
+}
 function ClubBooks() {
+  const books: IBook[] = useMyQuery('/books.json');
   return (
     <>
-      <div>ClubBooks</div>;
+      <SearchHeader search={false} text="미라클 모닝 독서" />
+      <div className="club-books-container">
+        {books?.map(book => (
+          <div key={book.id} className="book">
+            <div>
+              <h3>{book.title}</h3>
+            </div>
+            <img src={book.image_url} alt="" />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
