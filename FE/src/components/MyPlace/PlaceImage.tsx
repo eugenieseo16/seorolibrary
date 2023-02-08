aa777a08cde509de8c3b2bde1b636a84e546a2f1
yujinseo <00yujin00@naver.com>
2023-02-08 15:43:57 +0900

    ✨ feat : place리뷰작성 폼 생성

--- FE/src/components/MyPlace/PlaceImage.tsx
+++ FE/src/components/MyPlace/PlaceImage.tsx
@@ -1,27 +1,57 @@
 import React from 'react';
+import { Image } from 'antd';
 
-import { useMyQuery } from '@src/hooks/useMyQuery';
 import { MdLocalCafe } from 'react-icons/md';
+import { Col, Row } from 'antd';
 
 import './PlaceImage.styles.scss';
 
 function PlaceImage() {
-  const image_url =
-    'https://ldb-phinf.pstatic.net/20221129_186/1669714890383a4Ysc_JPEG/KakaoTalk_20221116_150311430.jpg';
   const title = '가게 이름';
   const description =
-    '가게 설명 프엔으로 해도 된다고 하겠지만 유니티 개발 희망자가 있을지 모르겠네 근데프엔으로 날고 기어봤자 유니티 못이김';
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae modi id nobis ex necessitatibus magnam officia, harum eveniet, distinctio praesentium provident corporis odio recusandae sunt accusamus. Quidem et ducimus exercitationem.';
+
   return (
     <div className="place-desciption-container">
+      {/* 장소 사진 분할 */}
       <div className="place-detail-image-container">
-        <img src={image_url} alt="" />
+        <Image.PreviewGroup>
+          <Row gutter={[4, 4]}>
+            {/* 왼쪽 1장 */}
+            <Col span={12}>
+              <Image src="/src/assets/images/place_sample1.jpeg" alt="" />
+            </Col>
+            {/* 오른쪽 4장 */}
+            <Col span={12}>
+              <Row gutter={[2, 2]}>
+                {/* 위 2장 */}
+                <Col span={12}>
+                  <Image src="/src/assets/images/place_sample2.jpg" alt="" />
+                </Col>
+                <Col span={12}>
+                  <Image src="/src/assets/images/place_sample3.jpg" alt="" />
+                </Col>
+                {/* 아래 2장 */}
+                <Col span={12}>
+                  <Image src="/src/assets/images/place_sample4.jpeg" alt="" />
+                </Col>
+                <Col span={12}>
+                  <Image src="/src/assets/images/place_sample5.jpg" alt="" />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Image.PreviewGroup>
       </div>
+      {/* 장소 설명 */}
       <div className="place-detail-content-container">
+        {/* 장소 이름 */}
         <h2>
           <MdLocalCafe />
           &nbsp;
           {title}
         </h2>
+        {/* 장소 설명 */}
         <h6>{description}</h6>
       </div>
     </div>
