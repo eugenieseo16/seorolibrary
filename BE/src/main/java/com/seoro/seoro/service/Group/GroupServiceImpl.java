477bb7999001f213c2be64ee9d9ebfc50a2c6791
cjfgns <cjfgns1997@naver.com>
2023-02-08 13:44:04 +0900

    :sparkles: Recommend user/group
    Recommend user/group

--- BE/src/main/java/com/seoro/seoro/service/Group/GroupServiceImpl.java
+++ BE/src/main/java/com/seoro/seoro/service/Group/GroupServiceImpl.java
@@ -1,11 +1,15 @@
 package com.seoro.seoro.service.Group;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
 import com.seoro.seoro.domain.dto.Group.GroupDetailResponseDto;
 import com.seoro.seoro.domain.dto.Group.GroupMainResponseDto;
+import com.seoro.seoro.domain.dto.Group.GroupShowDto;
+import com.seoro.seoro.domain.dto.Member.RecommendMemberDto;
 import com.seoro.seoro.domain.entity.Groups.GroupJoin;
 import com.seoro.seoro.repository.ChatRoom.ChatRoomRepository;
 
@@ -40,18 +44,69 @@ public class GroupServiceImpl implements GroupService{
 		GroupMainResponseDto groupMainResponseDto = new GroupMainResponseDto();
 		System.out.println("userName = " + userName);
 		//현재 로그인한 사용자
-		Member findMember = userRepository.findUserByMemberName(userName);
-		System.out.println("findUser = " + findMember);
+		Member findMember = userRepository.findByMemberName(userName);
+		System.out.println("findMember = " + findMember);
 
-		//같은 동코드를 가진 독서모임 반환
-		// String myDongCode = findUser.getUserDongCode();
-		// List<Groups> findMyDongGroups = groupRepository.findGroupsByGroupDongCode(myDongCode);
-		// if(findMyDongGroups.size() > 0) {
-		// 	groupMainResponseDto.setRecommendGroups(findMyDongGroups);
-		// }
+		//내 동코드와 장르를 받아옴
+		String myDongCode = findMember.getMemberDongCode();
+		Long myGenre = findMember.getMemberGenre();
 
-		//같은 동코드를 가진 사용자들 반환
+		//같은 동코드를 가진 독서모임 추천순으로 반환
+		List<Groups> dongGroups = groupRepository.findGroupsByGroupDongCode(myDongCode);
+		if(dongGroups.size() > 0) {
+			Collections.sort(dongGroups, new Comparator<Groups>(){
+				@Override
+				public int compare(Groups o1, Groups o2) {
+					Long o1_genre = o1.getGroupGenre();
+					Long o2_genre = o2.getGroupGenre();
+					if(Long.bitCount(o1_genre&myGenre) == Long.bitCount(o2_genre&myGenre)){
+						return (int)(Long.bitCount(o1_genre)-Long.bitCount(o2_genre));
+					}
+					return (int)(Long.bitCount(o2_genre&myGenre)-Long.bitCount(o1_genre&myGenre));
+				}
+			});
+			List<GroupShowDto> recommendGroups = new ArrayList<>();
+			for(Groups group : dongGroups){
+				if((group.getGroupGenre()&myGenre)==0){
+					break;
+				}
+				recommendGroups.add(GroupShowDto.builder()
+						.groupName(group.getGroupName())
+						.groupDescrib(group.getGroupIntroduction())
+						.groupProfile(group.getGroupProfile())
+						.build());
+			}
+			groupMainResponseDto.setRecommendGroups(recommendGroups);
+		}
 
+
+		//같은 동코드를 가진 사용자들 추천순으로 반환
+		List<Member> dongMember = userRepository.findByMemberDongCode(myDongCode);
+		if(dongMember.size()>0){
+			Collections.sort(dongMember, new Comparator<Member>(){
+				@Override
+				public int compare(Member o1, Member o2) {
+					Long o1_genre = o1.getMemberGenre();
+					Long o2_genre = o2.getMemberGenre();
+					if(Long.bitCount(o1_genre&myGenre) == Long.bitCount(o2_genre&myGenre)){
+						return (int)(Long.bitCount(o1_genre)-Long.bitCount(o2_genre));
+					}
+					return (int)(Long.bitCount(o2_genre&myGenre)-Long.bitCount(o1_genre&myGenre));
+				}
+			});
+			List<RecommendMemberDto> recommendMembers = new ArrayList<>();
+			for(Member member : dongMember){
+				if(member.getMemberName().equals(userName)) continue;
+				if((member.getMemberGenre()&myGenre)==0){
+					break;
+				}
+				recommendMembers.add(RecommendMemberDto.builder()
+					.memberProfile(member.getMemberProfile())
+					.memberName(member.getMemberName())
+					.build());
+			}
+			groupMainResponseDto.setRecommendMembers(recommendMembers);
+		}
 
 		//내가 참여하고 있는 독서모임 반환
 		List<GroupJoin> findGroupJoin = findMember.getGroupJoins();
@@ -59,17 +114,19 @@ public class GroupServiceImpl implements GroupService{
 		// for (int i=0; i<findGroupJoin.size(); i++) {
 		// 	System.out.println(findGroupJoin.get(i));
 		// }
-		List<GroupDetailResponseDto> myGroups = new ArrayList<>();
+		List<GroupShowDto> myGroups = new ArrayList<>();
 		for (GroupJoin groupJoin : findGroupJoin) {
 			Groups groups = groupJoin.getGroups();
-			myGroups.add(groupDetail(groups.getGroupId()));
-
+			myGroups.add(GroupShowDto.builder()
+					.groupProfile(groups.getGroupProfile())
+					.groupDescrib(groups.getGroupIntroduction())
+					.groupName(groups.getGroupName())
+				.build());
 		}
-
 		if(myGroups.size() > 0) {
 			groupMainResponseDto.setMyGroups(myGroups);
 		}
-		// groupMainResponseDto.setMyGroups(findGroupJoin);
+
 		groupMainResponseDto.setResult(true);
 		return groupMainResponseDto;
 	}
