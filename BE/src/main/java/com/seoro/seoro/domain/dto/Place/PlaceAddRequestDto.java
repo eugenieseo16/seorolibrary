3796c8965974d7802c69575adc5aca1aba26bf8a
cjfgns <cjfgns1997@naver.com>
2023-02-09 16:04:06 +0900

    :sparkles: Make Place
    Make Place

---
+++ BE/src/main/java/com/seoro/seoro/domain/dto/Place/PlaceAddRequestDto.java
@@ -0,0 +1,17 @@
+package com.seoro.seoro.domain.dto.Place;
+
+import com.seoro.seoro.domain.entity.Place.PlacePhoto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class PlaceAddRequestDto {
+	private String placeName;
+	private Long placeMaker;
+	private String placedescrib;
+	private String placePhoto;
+	private String latitude;
+	private String longitude;
+}
