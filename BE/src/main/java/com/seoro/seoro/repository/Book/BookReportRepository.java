cb64aa48d938e60241cad624600a2ad5de084a53
윤혜진 <hjyoon0223@gmail.com>
2023-02-09 20:12:22 +0900

    🐛 fix : run Exception 해결

--- BE/src/main/java/com/seoro/seoro/repository/Book/BookReportRepository.java
+++ BE/src/main/java/com/seoro/seoro/repository/Book/BookReportRepository.java
@@ -9,5 +9,4 @@ import com.seoro.seoro.domain.dto.Book.BookReportDto;
 import com.seoro.seoro.domain.entity.Book.BookReport;
 
 public interface BookReportRepository extends JpaRepository<BookReport, Long> {
-	List<BookReportDto> findBookReportsByMemberId(Long memberId);
 }
