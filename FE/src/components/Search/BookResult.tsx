f6ca4b43cfe377782e08e2186954e2f99bae1aa7
yeonjinnieoh <yeonjinnieoh@gmail.com>
2023-02-09 22:13:19 +0900

    🐛 fix : navigation 수정

--- FE/src/components/Search/BookResult.tsx
+++ FE/src/components/Search/BookResult.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useMyQuery } from '@src/hooks/useMyQuery';
 
 import './BookResult.styles.scss';
 function BookResult() {
   const data = useMyQuery('/books.json');
+  const navigate = useNavigate();
 
   return (
     <div className="book-result-container">
       {data?.map((book: any, i: number) => (
-        <div key={i} className="book-result-item">
+        <div
+          key={i}
+          className="book-result-item"
+          onClick={() => navigate(`/book/1`)}
+        >
           <img src={book.image_url} alt="" />
 
           <div className="book-detail-container">
