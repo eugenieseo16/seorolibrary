141ce4180292ab293f352956ac49551804a7e78e
하훈목 <hunmok1027@gmail.com>
2023-02-09 10:23:01 +0900

    ✨ feat : 기존 캐러셀 스타일

--- FE/src/components/Carousel/CarouselPlace.tsx
+++ FE/src/components/Carousel/CarouselPlace.tsx
@@ -4,7 +4,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './CarouselPlace.styles.scss';
-import { useQuery } from 'react-query';
 
 export interface ICarouselPlaceProps {
   items: Item[];
