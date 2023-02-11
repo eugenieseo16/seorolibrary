70db53a229e62c193201fc092a0084619ea0acf5
EveKritinLee <jylisa0612@naver.com>
2023-02-11 19:01:37 +0900

    :sparkles: feat : 독서모임 일정 수정
    feat : 독서모임 일정 수정

--- BE/src/main/java/com/seoro/seoro/domain/dto/Group/GroupScheduleUpdateRequestDto.java
+++ BE/src/main/java/com/seoro/seoro/domain/dto/Group/GroupScheduleUpdateRequestDto.java
@@ -5,13 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class GroupScheduleCreateRequestDto {
+public class GroupScheduleUpdateRequestDto {
+    private Long groupScheduleId;
     private Long groupId;
     private Long writerId;
     private String groupScheduleTitle;
