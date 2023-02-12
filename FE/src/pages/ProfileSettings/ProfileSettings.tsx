b24d42fe79b549eba9cb31e7dd86d601503a6d3b
yeonjinnieoh <yeonjinnieoh@gmail.com>
2023-02-12 12:26:48 +0900

    ✨ feature : 비밀번호 변경 페이지 구현

--- FE/src/pages/ProfileSettings/ProfileSettings.tsx
+++ FE/src/pages/ProfileSettings/ProfileSettings.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import Header from '@components/Header/Header';
 import EditProfile from '@components/ProfileSettings/EditProfile';
 import SettingItem from '@components/ProfileSettings/SettingItem';
 
 function ProfileSettings() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <Header text="프로필 설정" />
       <EditProfile />
-      <SettingItem text="비밀번호 변경" />
+
+      <span onClick={() => navigate('password')}>
+        <SettingItem text="비밀번호 변경" />
+      </span>
+
       <SettingItem text="공지사항" />
       <SettingItem text="이용약관" />
       <SettingItem text="로그아웃" />
