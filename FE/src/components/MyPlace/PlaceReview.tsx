99f76876629269bd2304c58d4d4200fa585c97e9
00yujin00 <00yujin00@naver.com>
2023-02-14 12:49:39 +0900

    ✨ feat : 리뷰 별점 변경, 리뷰 길이 수정

--- FE/src/components/MyPlace/PlaceReview.tsx
+++ FE/src/components/MyPlace/PlaceReview.tsx
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
 
 import './PlaceReview.styles.scss';
 import AddPlaceReviewModal from './AddPlaceReviewModal';
 
 function PlaceReview() {
+  const navigate = useNavigate();
   const getBookReview = async () =>
     await (await fetch('/bookReview.json')).json();
   const { data } = useQuery('book-review', getBookReview);
+
+  const [showFullContent, setShowFullContent] = useState<{
+    [key: string]: boolean;
+  }>({});
 
   return (
     <div className="place-review-container">
@@ -25,11 +31,33 @@ function PlaceReview() {
         {data?.data?.map((review: any, i: number) => (
           <div className="place-review-item" key={i}>
             <div className="place-review-user-info">
-              <img src={review.profile_img} alt="" />
-              <p>{review.nickname}</p>
+              <img
+                src={review.profile_img}
+                alt=""
+                onClick={() => navigate(`/profile/${i}`)}
+              />
+              <p onClick={() => navigate(`/profile/${i}`)}>{review.nickname}</p>
             </div>
             <div className="place-review-detail">
-              <a>{review.content}</a>
+              <a>
+                {showFullContent[review.id]
+                  ? review.content
+                  : review.content.substring(
+                      0,
+                      Math.min(review.content.length, 70),
+                    ) + '...'}
+              </a>
+              <a
+                onClick={() =>
+                  setShowFullContent({
+                    ...showFullContent,
+                    [review.id]: !showFullContent[review.id],
+                  })
+                }
+                className="place-review-more"
+              >
+                {showFullContent[review.id] ? '[닫기]' : '[더보기]'}
+              </a>
             </div>
           </div>
         ))}
