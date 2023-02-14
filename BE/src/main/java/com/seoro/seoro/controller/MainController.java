eb74b37e878eebde091c8dbfd149c9a089bc24ca
이지영 <jylisa0612@naver.com>
2023-02-14 10:20:19 +0900

    :sparkles: feat : 장르 반환
    feat : 장르 반환

--- BE/src/main/java/com/seoro/seoro/controller/MainController.java
+++ BE/src/main/java/com/seoro/seoro/controller/MainController.java
@@ -12,8 +12,10 @@ import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seoro.seoro.domain.dto.GenreResponseDto;
 import com.seoro.seoro.service.Book.BookService;
 import com.seoro.seoro.service.Member.MemberService;
 
@@ -60,5 +62,10 @@ public class MainController {
 			result.put("books",books);
 		}
 		return result;
+	}
+
+	@GetMapping("/genres")
+	public GenreResponseDto getGenres(@RequestParam("genres") int[] genres) {
+		return memberService.getGenres(genres);
 	}
 }
