49957e8729cf44594e8b09299f9d22d697393a11
이지영 <jylisa0612@naver.com>
2023-02-14 10:20:19 +0900

    :sparkles: feat : 장르 반환
    feat : 장르 반환

--- BE/src/main/java/com/seoro/seoro/domain/entity/Genre.java
+++ BE/src/main/java/com/seoro/seoro/domain/entity/Genre.java
@@ -4,9 +4,37 @@ import javax.persistence.*;
 
 import java.io.Serializable;
 
-@Entity
-public class Genre implements Serializable {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long genreId;
-    private String genreName;
+public enum Genre implements Serializable {
+    GJ("0", "경제"),
+    JGGB("1", "자기계발"),
+    S("2", "시/에세이"),
+    IM("3", "인문"),
+    JG("4", "종교"),
+    SS("5", "소설"),
+    KO("6", "국어/외국어"),
+    JC("7", "정치/사회"),
+    YS("8", "역사/문화"),
+    GH("9", "과학/공학"),
+    IT("10", "IT/프로그래밍"),
+    GG("11", "건강/의학"),
+    SH("12", "가정/생활/요리"),
+    YH("13", "여행/취미"),
+    DJ("14", "예술/대중문화"),
+    YA("15", "유아");
+
+    private final String idx;
+    private final String symbol;
+
+    Genre(String idx, String symbol) {
+        this.idx = idx;
+        this.symbol = symbol;
+    }
+
+    public String getIdx() {
+        return idx;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
 }
