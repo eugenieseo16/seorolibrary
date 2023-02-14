6aca5be5e7dde617d94ceb9046841bee4f2e266f
yjohbjects <yjohbjects@gmail.com>
2023-02-14 16:24:22 +0900

    💩 feat : 도서 리뷰 생성 삭제

--- FE/src/components/BookInfo/BookReview.tsx
+++ FE/src/components/BookInfo/BookReview.tsx
@@ -1,16 +1,41 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
-import { RiAddFill, RiSubtractFill } from 'react-icons/ri';
+import {
+  RiAddFill,
+  RiSubtractFill,
+  RiEdit2Line,
+  RiDeleteBinLine,
+} from 'react-icons/ri';
 
 import './BookReview.styles.scss';
-import { bookReviewAPI } from '@src/API/bookAPI';
+import {
+  bookReviewAPI,
+  bookReviewCreateAPI,
+  bookReviewDeleteAPI,
+  bookDetailAPI,
+} from '@src/API/bookAPI';
+import { useUser } from '@src/hooks/useUser';
+import { useMutation } from 'react-query';
+import { bookApiUrls } from '@src/API/apiUrls';
 
 interface IBookReviewProps {
   isbn: string;
 }
+
 function BookReview({ isbn }: IBookReviewProps) {
   const reviews = bookReviewAPI(isbn);
+  const bookData = bookDetailAPI(isbn);
+  const user = useUser();
+  const bookTitle = bookData?.bookTitle;
+  const bookImage = bookData?.bookImage;
+  const memberName = user?.memberName;
+  const { mutate } = useMutation(
+    `${bookApiUrls.editBookReview}/${isbn}`,
+    () => {
+      axios;
+    },
+  );
 
   const [toReview, setToReview] = useState(true);
 
@@ -19,8 +44,28 @@ function BookReview({ isbn }: IBookReviewProps) {
   const getChangeHandlerWithEvent = (name: string) => (e: any) =>
     setValue(name, e.target.value);
 
-  const onValid = (data: any) => {
-    console.log(data);
+  // 리뷰 생성
+  const onValid = (values: any) => {
+    setToReview(false);
+    const { data: response }: any = bookReviewCreateAPI({
+      isbn,
+      bookTitle,
+      bookImage,
+      memberName,
+      reviewContent: values?.reviewContent,
+    });
+    mutate();
+  };
+
+  // 리뷰 삭제
+  const toDelete = (review: any) => {
+    const { data: response }: any = bookReviewDeleteAPI(
+      {
+        memberName,
+        reviewId: review?.reviewId,
+      },
+      isbn,
+    );
   };
 
   return (
@@ -45,11 +90,8 @@ function BookReview({ isbn }: IBookReviewProps) {
             <form onSubmit={handleSubmit(onValid)}>
               <div className="create-review-header">
                 <div className="user-detail-section">
-                  <img
-                    src="https://enewstoday.co.kr/news/photo/201706/1075992_238703_2853.jpg"
-                    alt=""
-                  />
-                  <span>우주최강미녀</span>
+                  <img src={user?.memberProfile} alt="" />
+                  <span>{user?.memberName}</span>
                 </div>
 
                 <div className="button-section">
@@ -64,9 +106,9 @@ function BookReview({ isbn }: IBookReviewProps) {
               </div>
 
               <textarea
-                name="review"
+                name="reviewContent"
                 placeholder="리뷰를 작성해주세요"
-                onChange={getChangeHandlerWithEvent('review')}
+                onChange={getChangeHandlerWithEvent('reviewContent')}
               ></textarea>
             </form>
           </div>
@@ -77,10 +119,33 @@ function BookReview({ isbn }: IBookReviewProps) {
       <div>
         {reviews?.reviews?.map((review: any, i: number) => (
           <div className="review-item" key={i}>
-            <div className="review-user-info">
-              <img src={review.memberProfile} alt="" />
-              <p>{review.memberName}</p>
-            </div>
+            {review?.memberName === user?.memberName ? (
+              <div
+                className="review-user-info-self"
+                style={{ display: 'flex' }}
+              >
+                <div className="review-user-info">
+                  <img src={review.memberProfile} alt="" />
+                  <p>{review.memberName}</p>
+                </div>
+
+                <div className="review-user-self-item">
+                  {/* 리뷰 수정 */}
+                  <div className="edit" onClick={() => {}}>
+                    <RiEdit2Line size={'1.5rem'} />
+                  </div>
+                  {/* 리뷰 삭제 */}
+                  <div className="delete" onClick={() => toDelete(review)}>
+                    <RiDeleteBinLine size={'1.5rem'} />
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div className="review-user-info">
+                <img src={review.memberProfile} alt="" />
+                <p>{review.memberName}</p>
+              </div>
+            )}
 
             <div className="review-detail">
               <p>{review.reviewContent}</p>
