85af8ea66bed26a98d35b09e69c85b39ce6c685e
윤혜진 <hjyoon0223@gmail.com>
2023-02-15 20:50:15 +0900

    🐛 bug: 빌드 에러

--- BE/src/main/java/com/seoro/seoro/domain/dto/Book/OwnBookDetailDto.java
+++ BE/src/main/java/com/seoro/seoro/domain/dto/Book/OwnBookDetailDto.java
@@ -16,8 +16,34 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class OwnBookDetailDto {
 	private boolean result;
-	private BookDetailDto bookDetailDto;
+	private String messege;
+	private String isbn;
+	private String bookTitle;
+	private String bookAuthor;
+	private String bookPublisher;
+	private String bookImage;
+	private String bookDescrib;
+	private String bookPubDate;
+	private Long countReader;
+	private Long countComment;
+	private Long countReview;
+
+	// 보유도서 란 정보
 	private String ownComment;
 	private boolean isOwn;
-	private List<OwnBookDto> ownBooks;
+	private List<OwnBookDto> ownBookList;
+
+	public OwnBookDetailDto(BookDetailDto bookDetailDto) {
+		this.isbn = bookDetailDto.getIsbn();
+		this.bookTitle = bookDetailDto.getBookTitle();
+		this.bookAuthor = bookDetailDto.getBookAuthor();
+		this.bookPublisher = bookDetailDto.getBookPublisher();
+		this.bookImage = bookDetailDto.getBookImage();
+		this.bookDescrib = bookDetailDto.getBookDescrib();
+		this.bookPubDate = bookDetailDto.getBookPubDate();
+		this.countReader = bookDetailDto.getCountReader();
+		this.countComment = bookDetailDto.getCountComment();
+		this.countReview = bookDetailDto.getCountReview();
+	}
+
 }
