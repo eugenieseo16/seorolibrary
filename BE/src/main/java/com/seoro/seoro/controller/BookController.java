07744fe0b3c66a7b9ac6af68361636efe1c0a28d
윤혜진 <hjyoon0223@gmail.com>
2023-02-15 17:57:18 +0900

    ✨ feat : 사용자 보유도서 상세 조회

--- BE/src/main/java/com/seoro/seoro/controller/BookController.java
+++ BE/src/main/java/com/seoro/seoro/controller/BookController.java
@@ -1,13 +1,10 @@
 package com.seoro.seoro.controller;
 
-import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Map;
 
 import com.seoro.seoro.domain.dto.Book.*;
 import org.json.simple.parser.ParseException;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,9 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.seoro.seoro.domain.dto.Member.MemberDto;
 import com.seoro.seoro.domain.dto.ResultResponseDto;
 import com.seoro.seoro.service.Book.BookService;
 
@@ -37,6 +34,13 @@ public class BookController {
 		ParseException,
 		URISyntaxException {
 		return bookService.viewBookDetail(isbn,requestDto.getMemberId());
+	}
+
+	// 사용자 보유도서 조회
+	@GetMapping("/detail/own")
+	public OwnBookDetailDto viewOwnBookDetail(@RequestParam("memberName") String memberName, @RequestParam("isbn") String isbn)
+		throws ParseException, URISyntaxException {
+		return bookService.viewOwnBookDetail(memberName, isbn);
 	}
 
 	//읽은 도서 추가
